ce48372cb900ab7011a7c5a46ce1a9f8fcf55559
oerlex <alex.sp@posteo.de>
2015-10-08 16:44:17 +0200

    Added the validator class

---
+++ TheYachtClub/TheYachtClub/Controller/Validator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheYachtClub.Controller
+{
+    /*
+     *The validator class takes care that the user inputs are valid 
+     */
+    class Validator
+    {
+        /*
+        *Validates the member name in terms of length and
+        */
+        public static bool validateMembername(string name){
+           if(name.Length >=20)
+                return false;
+           return true;
+        }
+
+        /*
+       *Validates the personal number. The length can't have more or less than 10 digits
+       */
+        public static bool validatePersonalnumber(string personalNumber){
+            if (personalNumber.Length != 10)
+                return false;
+            return true;
+        }
+    }
+}
